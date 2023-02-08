77add57c7b8292e4d9f65900c5af8b33307d028d
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-02-08 09:46:42 +0530

    Classlist.toggle feature is used here

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
+let btn = document.getElementById('swap');
+let mainDiv = document.getElementById('app');
 
+
+btn.addEventListener('click', ()=>{
+    mainDiv.classList.toggle('night');
+    btn.classList.toggle('button_night')
+})
